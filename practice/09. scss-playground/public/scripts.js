9aa2fbd0cd1e8e6575fc746b37953e75bb36260e
Kobi Hari <kobi2294@yahoo.com>
2025-02-24 16:52:49 +0200

    playground ready for section 5

--- practice/09. scss-playground/public/scripts.js
+++ practice/09. scss-playground/public/scripts.js
@@ -5,4 +5,27 @@ function getWebSocketAddress() {
     return address;    
 }
 
+function connectWebSocket(callback) {
+    callback();
+    var address = getWebSocketAddress();
+    var socket = new WebSocket(address);
+
+    socket.onmessage = callback;
+}
+
+function loadCss(elementId) {
+    console.log('Refreshing css');
+    fetch(`./styles.css?cacheBust=${new Date().getTime()}`)
+    .then(response => response.text())
+    .then(css => {
+        const codeBlock = document.getElementById(elementId);
+        codeBlock.textContent = css;
+        Prism.highlightElement(codeBlock); // Apply syntax highlighting
+    })
+    .catch(error => {
+        document.getElementById(elementId).textContent = 'Error loading CSS: ' + error;
+    });
+
+}
+
 
