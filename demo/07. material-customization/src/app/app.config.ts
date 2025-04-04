ad24e331438c66cc8399f71434d0be415b4372d7
Kobi Hari <kobi2294@yahoo.com>
2025-04-04 03:11:04 +0300

    S09L13 - Custom SVG Icons

--- demo/07. material-customization/src/app/app.config.ts
+++ demo/07. material-customization/src/app/app.config.ts
@@ -1,8 +1,18 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, inject, provideAppInitializer, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideHttpClient } from '@angular/common/http';
+import { InitService } from './services/init.service';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }), 
+    provideRouter(routes),
+    provideHttpClient(), 
+    provideAppInitializer(() => {
+      inject(InitService).registerIcons();
+    })
+
+  ]
 };
---
+++ demo/07. material-customization/src/app/services/init.service.ts
@@ -0,0 +1,17 @@
+import { inject, Injectable } from '@angular/core';
+import { MatIconRegistry } from '@angular/material/icon';
+import { DomSanitizer } from '@angular/platform-browser';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class InitService {
+  readonly iconRegistey = inject(MatIconRegistry);
+  readonly domSanitizer = inject(DomSanitizer);
+
+  registerIcons() {
+    this.iconRegistey.addSvgIcon('trophy', this.domSanitizer.bypassSecurityTrustResourceUrl('svg/cup-star.svg'));
+    this.iconRegistey.addSvgIcon('half=heart', this.domSanitizer.bypassSecurityTrustResourceUrl('svg/heart-halfs.svg'));
+  }
+
+}
